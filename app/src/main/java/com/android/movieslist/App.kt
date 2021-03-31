415ab4f18489db1fe026206a46ca6d32b3e408d8
Rudem <ernesoglu@gmail.com>
2021-03-31 14:53:16 +0300

    Initial commit

---
+++ app/src/main/java/com/android/movieslist/App.kt
@@ -0,0 +1,10 @@
+package com.android.movieslist
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/android/movieslist/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.movieslist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.movieslist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
